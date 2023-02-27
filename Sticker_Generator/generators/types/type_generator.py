23f70d717389e62640d3d9c983c3fc9e1dfcd0e6
Lucas <lucas.ewing2000@gmail.com>
2023-02-27 13:57:33 -0800

    Lint fixes

---
+++ Sticker_Generator/generators/types/type_generator.py
@@ -0,0 +1,13 @@
+import random
+
+# Sticker Types
+stickerTypes = ["funny giraffe cartoon", "harry potter cartoon", "great dane cartoon"]
+
+
+# Select sticker type
+def random_sticker() -> str:
+    """Returns a random sticker type"""
+    # Randomly select sticker type
+    sticker = random.randrange(0, len(stickerTypes))
+    print("Generating a " + stickerTypes[sticker])
+    return stickerTypes[sticker]
