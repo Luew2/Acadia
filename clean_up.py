21b87036d43484ce6b9cd7907c0f2a16085a3b4c
Lucas <lucas.ewing2000@gmail.com>
2023-03-12 16:12:47 -0700

    Refactored some code

--- clean_up.py
+++ clean_up.py
@@ -1,11 +1,9 @@
 import os
 
 path = r"./Sticker_Generator/data/"
-print(path)
+print("Deleting files in " + path + "...")
 for file_name in os.listdir(path):
     # construct full file path
     file = path + file_name
-    print(file)
     if os.path.isfile(file):
-        print("Deleting file:", file)
         os.remove(file)
