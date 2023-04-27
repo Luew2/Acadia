2b1cea1e229cfe218a37e217ffef76297fef24ff
Lucas <lucas.ewing2000@gmail.com>
2023-04-26 19:15:46 -0700

    deleted

--- clean_up.py
+++
@@ -1,9 +0,0 @@
-import os
-
-path = r"./Sticker_Generator/data/"
-print("Deleting files in " + path + "...")
-for file_name in os.listdir(path):
-    # construct full file path
-    file = path + file_name
-    if os.path.isfile(file):
-        os.remove(file)
