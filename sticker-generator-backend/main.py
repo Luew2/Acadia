51409eb1ef6c0383353d8b90b1d8a658d0586ac6
Lucas <lucas.ewing2000@gmail.com>
2023-04-26 18:22:42 -0700

    reworked support for gcloud

--- sticker-generator-backend/main.py
+++ sticker-generator-backend/main.py
@@ -238,5 +238,5 @@ def main(stdscr):
     stdscr.getch()
 
 
-# if __name__ == "__main__":
-#     curses.wrapper(main)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 8080))
